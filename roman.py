82bfa59c40b6e4246c020fdaef099fd026811c80
Rajesh0045 <52398693+Rajesh0045@users.noreply.github.com>
2019-07-07 11:19:45 +0530

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,3 +1,4 @@
-duck=input()
-list=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
-print(list.index(duck)+1)
+data=input()
+roman=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+num = roman.index(data)
+print(num)
