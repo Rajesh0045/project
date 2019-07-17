1c9d9ed9356a49249b57bba087191d790aaa6722
Rajesh0045 <52398693+Rajesh0045@users.noreply.github.com>
2019-07-17 10:25:22 +0530

    Create unique.py

---
+++ unique.py
@@ -0,0 +1,9 @@
+import collections as c
+avail = input()
+n = list(map(int,input().split()))
+acr = c.Counter(n)
+same = []
+for item,counting in acr.items():
+    if counting >1 :
+        same.append(item)
+if same:
